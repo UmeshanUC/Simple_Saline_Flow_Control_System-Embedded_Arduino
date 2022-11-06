08fc74c1511f26a7a03d040c48da3b37d6b21461
Charitha-Jeewanka <cjrathnayake24@gmail.com>
2022-11-06 17:13:40 +0530

    7Seg Working

--- Final/Final.ino
+++ Final/Final.ino
@@ -26,23 +26,35 @@ byte rowPins[ROWS] = {0, 1, 2, 3};
 byte colPins[COLS] = {5, 6, 7}; 
 
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
-int keyNum = 0;
+// int keyNum = 0;
+uint8_t data[] = {0xff,0xff,0xff,0xff};
+uint8_t blank[] = {0x00,0x00,0x00,0x00};
+
 
 void setup(){
   Serial.begin(115200);
+  
+  display.setBrightness(0x0f);
   
 }
   
 void loop(){
   char customKey = customKeypad.getKey();
-  int keyNum = 0;
+  
+  //display.setSegments(data);
+  uint8_t keyNum = (uint8_t)customKey;
+  
   
   // Keypad
   if (customKey){
     Serial.println(customKey);
-    display.showNumberDec(customKey);
+    data[0] = display.encodeDigit(keyNum);
   }
-  
+    
+    data[1] = display.encodeDigit(0);
+    data[2] = display.encodeDigit(0);
+    data[3] = display.encodeDigit(0);    
+    display.setSegments(data);
 
   // TM1637
   /*if(customKey)
